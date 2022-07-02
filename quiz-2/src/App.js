607938e849dd027d61fe1014bb66ce84fdd620e4
aziz403 <azizbenmallouk4@gmail.com>
2022-07-02 15:56:52 +0100

    header style (quiz-2)

---
+++ quiz-2/src/App.js
@@ -0,0 +1,18 @@
+import Header from './header/index'
+import Section1 from './section-1/index'
+import Section2 from './section-2/index'
+import Footer from './footer/index'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Section1/>
+      <Section2/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ quiz-2/src/header/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <header>
+        <nav>
+            <a href='#'>Home</a>
+            <a href='#'>Contact us</a>
+            <a href='#'>0</a>
+            <a href='#'>Login</a>
+            <a href='#'>Sing Up</a>
+        </nav>
+        <div>
+            <h1>Origin</h1>
+            <p>Deep Sea Bio-Medical<br/>Research</p>
+            <button>Explore</button>
+        </div>
+    </header>
+  )
+}
+
+export default Index
